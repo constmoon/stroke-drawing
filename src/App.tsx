b9a5009aaf2be9a42a12473c295a49b575926966
constmoon <jiwoo.link@gmail.com>
2025-08-22 16:55:53 +0900

    vite 설정 및 tailwind 추가

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="App min-h-screen bg-gray-100 p-8">
+      <h2 className="text-lg text-gray-600 mb-6">
+        Start editing to see some magic happen!
+      </h2>
+      <div className="flex gap-3 items-center justify-center mb-6">test</div>
+    </div>
+  )
+}
